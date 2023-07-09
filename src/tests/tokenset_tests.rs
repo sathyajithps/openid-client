26195ceae9892d41c26d848ee530e203ba637797
Sathyajith P S <sathyajithsps@gmail.com>
2023-07-09 12:55:15 +0530

    Added TokenSet

---
+++ src/tests/tokenset_tests.rs
@@ -0,0 +1,71 @@
+use assert_json_diff::assert_json_eq;
+use serde_json::{json, Value};
+
+use crate::{helpers::now, tokenset::TokenSetParams};
+
+use super::TokenSet;
+
+#[test]
+fn sets_the_expire_at_automatically_from_expires_in() {
+    let tokenset = TokenSet::new(TokenSetParams {
+        expires_in: Some(300),
+        ..Default::default()
+    });
+
+    assert_eq!(Some(300), tokenset.get_expires_in());
+    assert_eq!(Some(now() + 300), tokenset.get_expires_at());
+    assert_eq!(false, tokenset.expired());
+}
+
+#[test]
+fn expired_token_sets_expires_in_to_0() {
+    let tokenset = TokenSet::new(TokenSetParams {
+        expires_in: Some(-30),
+        ..Default::default()
+    });
+
+    assert_eq!(Some(0), tokenset.get_expires_in());
+    assert_eq!(Some(now() - 30), tokenset.get_expires_at());
+    assert_eq!(true, tokenset.expired());
+}
+
+#[test]
+fn provides_a_claims_getter() {
+    let tokenset = TokenSet::new(TokenSetParams{
+        id_token: Some("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ".to_string()),
+        ..Default::default()
+    });
+
+    let claims = tokenset.claims().unwrap();
+
+    assert_eq!(Some(&json!("1234567890")), claims.get("sub"));
+    assert_eq!(Some(&json!("John Doe")), claims.get("name"));
+    assert_eq!(Some(&json!(true)), claims.get("admin"));
+}
+
+#[test]
+fn claims_throws_if_no_id_token_is_present() {
+    let tokenset = TokenSet::new(TokenSetParams::default());
+
+    let error = tokenset.claims().unwrap_err();
+
+    assert!(error.is_type_error());
+    assert_eq!(
+        "id_token not present in TokenSet",
+        error.type_error().error.message
+    );
+}
+
+#[test]
+fn claims_does_not_extend_dumped_tokenset_properties() {
+    let tokenset = TokenSet::new(TokenSetParams{
+        id_token: Some("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ".to_string()),
+        ..Default::default()
+    });
+    let tokenset_str = serde_json::to_string(&tokenset).unwrap();
+
+    assert_json_eq!(
+        json!({"id_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"}),
+        serde_json::from_str::<Value>(&tokenset_str).unwrap()
+    );
+}
---
+++ src/tokenset.rs
@@ -0,0 +1,191 @@
+use std::{cmp::max, collections::HashMap, num::Wrapping};
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::{helpers::now, types::OidcClientError};
+
+/// # TokenSetParams
+/// Argument to create new TokenSetParams
+#[derive(Debug, Default)]
+pub struct TokenSetParams {
+    /// `access_token`
+    pub access_token: Option<String>,
+    /// `token_type`
+    pub token_type: Option<String>,
+    /// `id_token`
+    pub id_token: Option<String>,
+    /// `refresh_token`
+    pub refresh_token: Option<String>,
+    /// `expires_in` - Access token expiration in (seconds)
+    pub expires_in: Option<i64>,
+    /// `expires_at` - Access token expiration timestamp, represented as the number of seconds since the epoch
+    pub expires_at: Option<i64>,
+    /// `session_state`
+    pub session_state: Option<String>,
+    /// `scope`
+    pub scope: Option<String>,
+    /// `other`
+    pub other: Option<HashMap<String, Value>>,
+}
+
+/// # TokenSet
+/// Represents a set of tokens retrieved from either authorization callback or successful token endpoint grant call.
+/// - If there are other properties present, it will be stored in `other` field. Access it via [`TokenSet::get_other()`]
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct TokenSet {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    access_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    token_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    id_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    refresh_token: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    expires_in: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    expires_at: Option<i64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    session_state: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    scope: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    other: Option<HashMap<String, Value>>,
+}
+
+impl TokenSet {
+    /// # Create a [TokenSet] instance
+    ///
+    /// - `expires_at` - Access token expiration timestamp, represented as the number of seconds since the epoch
+    pub fn new(params: TokenSetParams) -> Self {
+        let mut tokenset = Self {
+            access_token: params.access_token,
+            token_type: params.token_type,
+            id_token: params.id_token,
+            refresh_token: params.refresh_token,
+            expires_in: params.expires_in,
+            expires_at: params.expires_at,
+            session_state: params.session_state,
+            scope: params.scope,
+            other: params.other,
+        };
+
+        if params.expires_at.is_none() && params.expires_in.is_some() {
+            if let Some(e) = params.expires_in {
+                tokenset.expires_at = Some((Wrapping(now()) + Wrapping(e)).0);
+            }
+        }
+
+        if let Some(e) = params.expires_in {
+            if e < 0 {
+                tokenset.expires_in = Some(0);
+            }
+        }
+
+        tokenset
+    }
+
+    /// Returns if the set is expired or not
+    pub fn expired(&self) -> bool {
+        let expires_in = self.get_expires_in_internal();
+
+        if let Some(e) = expires_in {
+            return e == 0;
+        }
+        false
+    }
+
+    /// Get claims from the id_token
+    /// - This method just decodes and returns the found claims. Does not validate
+    pub fn claims(&self) -> Result<HashMap<String, Value>, OidcClientError> {
+        if self.id_token.is_none() {
+            return Err(OidcClientError::new_type_error(
+                "id_token not present in TokenSet",
+                None,
+            ));
+        }
+
+        let id_token_components: Vec<&str> = self.id_token.as_ref().unwrap().split('.').collect();
+        let payload = id_token_components.get(1);
+
+        if payload.is_none() {
+            return Err(OidcClientError::new_type_error(
+                "id_token invalid. payload component not found",
+                None,
+            ));
+        }
+
+        match base64_url::decode(payload.unwrap()) {
+            Ok(decoded) => {
+                serde_json::from_slice::<HashMap<String, Value>>(&decoded).map_err(|_| {
+                    OidcClientError::new_type_error("id_token payload is not a json object", None)
+                })
+            }
+            Err(_) => Err(OidcClientError::new_type_error(
+                "id_token payload is not base64url encoded",
+                None,
+            )),
+        }
+    }
+
+    /// Gets the access token
+    pub fn get_access_token(&self) -> Option<String> {
+        self.access_token.clone()
+    }
+
+    /// Gets the access token type
+    pub fn get_token_type(&self) -> Option<String> {
+        self.token_type.clone()
+    }
+
+    /// Gets the raw id token
+    pub fn get_id_token(&self) -> Option<String> {
+        self.id_token.clone()
+    }
+
+    /// Gets the refresh token
+    pub fn get_refresh_token(&self) -> Option<String> {
+        self.refresh_token.clone()
+    }
+
+    /// Gets the expires in
+    pub fn get_expires_in(&self) -> Option<i64> {
+        self.expires_in
+    }
+
+    /// Gets the expires in (seconds)
+    pub fn get_expires_at(&self) -> Option<i64> {
+        self.expires_at
+    }
+
+    /// Gets the session state from OP
+    pub fn get_session_state(&self) -> Option<String> {
+        self.session_state.clone()
+    }
+
+    /// Gets the scope
+    pub fn get_scope(&self) -> Option<String> {
+        self.scope.clone()
+    }
+
+    /// Gets the other fields
+    pub fn get_other(&self) -> Option<HashMap<String, Value>> {
+        self.other.clone()
+    }
+
+    // pub(self) fn set_expires_in(&mut self, val: i64) {
+    //     self.expires_in = Some((Wrapping(now()) + Wrapping(val)).0);
+    // }
+
+    pub(self) fn get_expires_in_internal(&self) -> Option<i64> {
+        if let Some(e) = self.expires_at {
+            return Some(max((Wrapping(e) - Wrapping(now())).0, 0));
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+#[path = "./tests/tokenset_tests.rs"]
+mod tokenset_tests;
