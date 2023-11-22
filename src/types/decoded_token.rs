fff13c67af31ba53e20ce24335fbd3e8e5ad90d5
Sathyajith P S <sathyajithsps@gmail.com>
2023-11-22 22:59:27 +0000

    Added docs to DecodedToken Type

--- src/types/decoded_token.rs
+++ src/types/decoded_token.rs
@@ -1,9 +1,14 @@
 use josekit::{jws::JwsHeader, jwt::JwtPayload};
 
+/// # DecodedToken
+/// The decoded jwt token
 #[derive(Debug)]
 pub struct DecodedToken {
+    /// See [josekit::jws::JwsHeader]
     pub header: JwsHeader,
+    /// See [josekit::jwt::JwtPayload]
     pub payload: JwtPayload,
+    /// Signature of jwt
     pub signature: String,
 }
 
